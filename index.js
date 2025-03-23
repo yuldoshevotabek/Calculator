942d60daed66d15d5135ca0d2c3a87b405402729
Otabek <otabekyuldoshev2007@gmail.com>
2025-03-23 15:32:49 +0500

    tayyor

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const btns = document.querySelectorAll('.btn button'),
 	minus = document.querySelector('#minus'),
 	boluv = document.querySelector('#boluv'),
 	kvadrat = document.querySelector('.kvadrat'),
-	foiz = document.querySelector('.foiz')
+	foiz = document.querySelector('.foiz'),
+	del = document.querySelector('#delete')
 console.log(btns)
 btns.forEach(item => {
 	item.addEventListener('click', () => {
@@ -17,46 +18,71 @@ clear.addEventListener('click', () => {
 	const text = (document.querySelector('#textt').textContent = '')
 })
 plus.addEventListener('click', () => {
-	window.ham = document.querySelector('#textt').textContent
-	const text = (document.querySelector('#textt').textContent = '+')
+	const text = (document.querySelector('#textt').textContent += '+')
 })
 minus.addEventListener('click', () => {
-	window.ham = document.querySelector('#textt').textContent
-	const text = (document.querySelector('#textt').textContent = '-')
+	const text = (document.querySelector('#textt').textContent += '-')
 })
 boluv.addEventListener('click', () => {
-	window.ham = document.querySelector('#textt').textContent
-	const text = (document.querySelector('#textt').textContent = '/')
+	const text = (document.querySelector('#textt').textContent += '/')
 })
 kvadrat.addEventListener('click', () => {
-	window.ham = document.querySelector('#textt').textContent
-	const text = (document.querySelector('#textt').textContent = 'x')
+	const text = (document.querySelector('#textt').textContent += 'x')
 })
 foiz.addEventListener('click', () => {
-	window.ham = document.querySelector('#textt').textContent
-	const text = (document.querySelector('#textt').textContent = '%')
+	const text = (document.querySelector('#textt').textContent += '%')
 })
 teng.addEventListener('click', () => {
 	const text = document.querySelector('#textt').textContent
-	if (text[0] == '+') {
+	if (text.includes('+') == true) {
 		const text = document.querySelector('#textt').textContent
-		let counter = Number(text) + Number(ham)
+		let counter = text
+			.split('+')
+			.map(Number)
+			.reduce((total, item) => {
+				return (total += item)
+			})
 		document.querySelector('#textt').innerHTML = counter
-	} else if (text[0] == '-') {
+	} else if (text.includes('-') == true) {
 		const text = document.querySelector('#textt').textContent
-		let counter = Number(ham) - Number(text.slice(1, text.length))
+		let counter = text
+			.split('-')
+			.map(Number)
+			.reduce((total, item) => {
+				return (total -= item)
+			})
 		document.querySelector('#textt').innerHTML = counter
-	} else if (text[0] == '/') {
+	} else if (text.includes('/') == true) {
 		const text = document.querySelector('#textt').textContent
-		let counter = Number(ham) / Number(text.slice(1, text.length))
+		let counter = text
+			.split('/')
+			.map(Number)
+			.reduce((total, item) => {
+				return (total /= item)
+			})
 		document.querySelector('#textt').innerHTML = counter
-	} else if (text[0] == 'x') {
+	} else if (text.includes('x') == true) {
 		const text = document.querySelector('#textt').textContent
-		let counter = Number(ham) * Number(text.slice(1, text.length))
+		let counter = text
+			.split('x')
+			.map(Number)
+			.reduce((total, item) => {
+				return (total *= item)
+			})
 		document.querySelector('#textt').innerHTML = counter
-	} else {
+	} else if (text.includes('%') == true) {
 		const text = document.querySelector('#textt').textContent
-		let counter = Number(ham) % Number(text.slice(1, text.length))
+		let counter = text
+			.split('%')
+			.map(Number)
+			.reduce((total, item) => {
+				return (total %= item)
+			})
 		document.querySelector('#textt').innerHTML = counter
 	}
+})
+del.addEventListener('click', () => {
+	const text = document.querySelector('#textt').textContent
+	let cler = text.slice(0, -1)
+	document.querySelector('#textt').innerHTML = cler
 })
