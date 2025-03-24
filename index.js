b631a0ae5758a411f36bb3c0f2b45c2e21545ee3
Otabek <otabekyuldoshev2007@gmail.com>
2025-03-24 11:43:37 +0500

    hato tuzatildi

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const btns = document.querySelectorAll('.btn button'),
 btns.forEach(item => {
 	item.addEventListener('click', () => {
 		const text = document.querySelector('#textt').textContent
-		if (text[0] == 0) {
+		if (
+			text[0] == 0 ||
+			text[0] == '+' ||
+			text[0] == '-' ||
+			text[0] == '*' ||
+			text[0] == '/' ||
+			text[0] == '%'
+		) {
 			const text = document.querySelector('#textt')
 			text.innerHTML = item.textContent
 		} else {
@@ -24,19 +31,69 @@ clear.addEventListener('click', () => {
 	document.querySelector('#textt').textContent = ''
 })
 plus.addEventListener('click', () => {
-	const text = (document.querySelector('#textt').textContent += '+')
+	const text = document.querySelector('#textt').textContent
+	if (
+		text.slice(-1) == '+' ||
+		text.slice(-1) == '-' ||
+		text.slice(-1) == '/' ||
+		text.slice(-1) == '*' ||
+		text.slice(-1) == '%'
+	) {
+	} else {
+		document.querySelector('#textt').textContent += '+'
+	}
 })
 minus.addEventListener('click', () => {
-	const text = (document.querySelector('#textt').textContent += '-')
+	const text = document.querySelector('#textt').textContent
+	if (
+		text.slice(-1) == '+' ||
+		text.slice(-1) == '-' ||
+		text.slice(-1) == '/' ||
+		text.slice(-1) == '*' ||
+		text.slice(-1) == '%'
+	) {
+	} else {
+		document.querySelector('#textt').textContent += '-'
+	}
 })
 boluv.addEventListener('click', () => {
-	const text = (document.querySelector('#textt').textContent += '/')
+	const text = document.querySelector('#textt').textContent
+	if (
+		text.slice(-1) == '+' ||
+		text.slice(-1) == '-' ||
+		text.slice(-1) == '/' ||
+		text.slice(-1) == '*' ||
+		text.slice(-1) == '%'
+	) {
+	} else {
+		document.querySelector('#textt').textContent += '/'
+	}
 })
 kvadrat.addEventListener('click', () => {
-	const text = (document.querySelector('#textt').textContent += '*')
+	const text = document.querySelector('#textt').textContent
+	if (
+		text.slice(-1) == '+' ||
+		text.slice(-1) == '-' ||
+		text.slice(-1) == '/' ||
+		text.slice(-1) == '*' ||
+		text.slice(-1) == '%'
+	) {
+	} else {
+		document.querySelector('#textt').textContent += '*'
+	}
 })
 foiz.addEventListener('click', () => {
-	const text = (document.querySelector('#textt').textContent += '%')
+	const text = document.querySelector('#textt').textContent
+	if (
+		text.slice(-1) == '+' ||
+		text.slice(-1) == '-' ||
+		text.slice(-1) == '/' ||
+		text.slice(-1) == '*' ||
+		text.slice(-1) == '%'
+	) {
+	} else {
+		document.querySelector('#textt').textContent += '%'
+	}
 })
 teng.addEventListener('click', () => {
 	const text = document.querySelector('#textt').textContent
